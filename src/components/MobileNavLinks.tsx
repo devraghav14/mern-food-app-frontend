efdab4d7974affb2636b503dce644e1f7af9c660
Hoax71 <devraghav538@gmail.com>
2024-08-31 22:47:41 +0530

    adding view order features

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -10,6 +10,12 @@ export const MobileNavLinks = () => {
         <Link to="/user-profile" className='flex bg-white items-center font-bold hover:text-orange-500'>
             User Profile
         </Link>
+        <Link to="/order-status" className='flex bg-white items-center font-bold hover:text-orange-500'>
+            Order Status
+        </Link>
+        <Link to="/manage-restaurant" className='flex bg-white items-center font-bold hover:text-orange-500'>
+            My Restaurant
+        </Link>
         <Button onClick={() => logout()} className='flex items-center px-3 font-bold hover:bg-gray-500'>
             Log Out
         </Button>
